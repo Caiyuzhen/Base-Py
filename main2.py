c374224673640d69c6c90d69c6fb9eb271f4c48e
yuzhen <caiyuzhen00@gmail.com>
2022-12-21 13:44:03 +0800

    base learning

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import requests
 
 # 请求 api
 response = requests.get('http://randomuser.me/api')
-response2 = requests.get('http://randomuser.me/api/?results=20')
+response2 = requests.get('http://randomuser.me/api/?results=5')
 
 data = response.json()
 data2 = response2.json()
@@ -14,3 +14,23 @@ for user in data2['results']:
 
 
 print(9 * 1000)
+
+
+# 创建函数
+def greet(greeting, name):
+	# 模板字符串
+	"""Return a greeting
+
+
+	Arguments:
+		greeting {string} -- A great world
+		name {string} -- A great person name
+
+
+	Returns:
+		string -- A great with a name
+	"""
+	return f'{greeting} {name}'
+
+
+print(greet('hello', 'world'))
---
+++ pytest/test_math.py
@@ -0,0 +1,12 @@
+def add(x, y):
+	return x + y
+
+def test_add():
+	assert add(1, 2) == 4 #断言这个函数的结果是4, 用作测试
+
+
+def subtract(x, y):
+	return x - y
+
+def test_subtract():
+	assert subtract(2, 1) == 3 #断言这个函数的结果是3, 用作测试
