b41a2c10d6d47e9d0793cbe27ba8e4fc83953eac
Caiyuzhen <caiyuzhen00@gmail.com>
2023-01-05 23:37:53 +0800

    base:learning

--- 23_Type.py
+++ 23_Type.py
@@ -34,3 +34,103 @@ print(d.__class__) #🌟查看对象类型的方法二
 
 # 🔥🔥类本身也是 type！git
 print(type(C)) # type
+
+
+
+print(' \n __分割线__ \n')
+
+
+# 🔥🔥利用 type 的方式来定义类
+# 普通的类定义的方式
+class C:
+	pass
+
+
+# type 定义类的方式, 第一个参数传入类名
+A = type('C', (), {}) # 第一个参数是类名, 第二个参数是父类(用于继承)并且第二个参数需要是个元祖类型所以要加个【, 逗号】, 第三个参数是类的属性和方法, 需要传入字典
+a = A() # a 是一个 C 类的实例
+
+print(a.__class__)
+print(A.__base__) # 因为没有指定类型, 所以 A 的父类是 object
+
+
+
+
+
+# type 定义类的方式, 第二个参数传入父类
+D = type('D', (C, ), {}) #⚡️⚡️第二个参数需要是个元祖类型所以要加个【, 逗号】!!
+d = D()
+print(d.__class__) # d 的类型是 D
+print(D.__bases__) # d 的父类是 C
+
+
+
+
+# type 定义类的方式, 第三个参数传入属性
+E = type('E', (), dict(x=1, y=2))
+
+
+
+
+
+# type 定义类的方式, 第三个参数传入函数
+def funC(self, name='world'):
+	print(f'hello {name}')
+
+F = type('F', (), dict(sayHi = funC)) # 第三个参数可以传入函数方法
+f = F()
+f.sayHi('Zen') #⚡️⚡️调用 F 类的 funC 方法并传入参数
+
+
+
+
+
+# __init_subclass__ 用于管理子类
+class CC:
+	def __init_subclass__(cls): #用于管理子类！！
+		print('你好~触发了父类')
+		cls.x = 996
+
+class DD(CC):
+	xx = 1
+
+print(DD.x) # 996 #还是父类的 x 属性， 因为上面调用了 __init_subclass__ 方法！子类没法修改其属性
+
+
+
+
+class GG:
+	def __init_subclass__(cls, value):
+		print('你好~触发了父类')
+		cls.x = value
+
+class KK(GG, value=996):#value 传给父类
+	x = 88 # 会被父类所拦截
+
+print(KK.x) # 996, ⚡️还是会被父类所拦截！
+
+
+
+
+
+#type 定义类的方式, 第四个参数传入【元类的属性】字典！
+class Glass:
+	def __init_subclass__(cls, value1, value2):
+		print('你好~触发了 Glass 父类')
+		cls.x = value1
+		cls.y = value2
+
+KP = type('KP', (Glass,), dict(x=3333), value1=777, value2=333) #value 传给父类
+print(KP.x) #👉🔥🔥不是 333, 🔥🔥而是会被父类拦截为 cls.x = value1 =  777!!
+
+
+
+
+
+
+
+
+
+
+
+
